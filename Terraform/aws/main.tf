df74c6ae61a7b1655d63bfc5ad02c5296d8e3b6d
Nakshatra Gowda <68193688+NakshatraGowda@users.noreply.github.com>
2025-11-10 08:07:43 +0530

    First terraform project

---
+++ Terraform/aws/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+  region = "us-east-1"  # <-- Change this to your preferred AWS region
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+ami = data.aws_ami.amazon_linux.id
+instance_type = "t3.micro"
+
+tags = {
+ Name = "Terraform_Demo"
+}
+}
